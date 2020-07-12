d07ef21256cc0f82dcb3a8f65cba0ddd2d350dc8
Meghan Denny <nektromc@gmail.com>
2020-07-11 20:58:34 -0400

    add metadata command

---
+++ cmd_metadata.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+type iaMeta struct {
+	A string   `xml:"identifier,omitempty" json:"identifier,omitempty"`
+	G string   `xml:"title,omitempty" json:"title,omitempty"`
+	B string   `xml:"mediatype,omitempty" json:"mediatype,omitempty"`
+	C []string `xml:"collection,omitempty" json:"collection,omitempty"`
+	D string   `xml:"description,omitempty" json:"description,omitempty"`
+	H string   `xml:"publicdate,omitempty" json:"publicdate,omitempty"`
+	I string   `xml:"uploader,omitempty" json:"uploader,omitempty"`
+	J string   `xml:"addeddate,omitempty" json:"addeddate,omitempty"`
+	L string   `xml:"backup_location,omitempty" json:"backup_location,omitempty"`
+	//
+	E string `xml:"scanner,omitempty" json:"scanner,omitempty"`
+	F string `xml:"subject,omitempty" json:"subject,omitempty"`
+	K string `xml:"curation,omitempty" json:"curation,omitempty"`
+	M string `xml:"external_metadata_update,omitempty" json:"external_metadata_update,omitempty"`
+	//
+	N string   `xml:"creator,omitempty" json:"creator,omitempty"`
+	O string   `xml:"hidden,omitempty" json:"hidden,omitempty"`
+	P []string `xml:"updater,omitempty" json:"updater,omitempty"`
+	Q []string `xml:"updatedate,omitempty" json:"updatedate,omitempty"`
+	R string   `xml:"nav_order,omitempty" json:"nav_order,omitempty"`
+	S string   `xml:"spotlight_identifier,omitempty" json:"spotlight_identifier,omitempty"`
+	T string   `xml:"publisher,omitempty" json:"publisher,omitempty"`
+	U string   `xml:"num_top_ba,omitempty" json:"num_top_ba,omitempty"`
+	V []string `xml:"related_collection,omitempty" json:"related_collection,omitempty"`
+	W string   `xml:"show_search_by_year,omitempty" json:"show_search_by_year,omitempty"`
+}
+
+var cmdMetadata = &cobra.Command{
+	Use:   "metadata",
+	Short: "retrieve metadata for items and collections",
+	Run: func(c *cobra.Command, args []string) {
+		assert(len(args) > 0, "missing item identifier")
+		name := args[0]
+		_, bys, ok := getDoc("https://archive.org/download/"+name+"/"+name+"_meta.xml", nil)
+		if !ok {
+			logError("errer finding metadata for item: " + name)
+			return
+		}
+		data := &iaMeta{}
+		xml.Unmarshal(bys, data)
+		jsn, _ := json.MarshalIndent(data, "", "    ")
+		fmt.Println(string(jsn))
+	},
+}
+
+func init() {
+	cmdRoot.AddCommand(cmdMetadata)
+}
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/nektro/go-util/ansi/style"
 	"github.com/spf13/cobra"
 )
@@ -36,4 +40,33 @@ func logError(err string) {
 	fmt.Print(style.FgRed)
 	log.Println(err)
 	fmt.Print(style.ResetFgColor)
+}
+
+func assert(b bool, msg string) {
+	if !b {
+		dieOnError(errors.New(msg))
+	}
+}
+
+func getDoc(urlS string, hdrs map[string]string) (*goquery.Document, []byte, bool) {
+	req, err := http.NewRequest(http.MethodGet, urlS, nil)
+	if err != nil {
+		return nil, nil, false
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, nil, false
+	}
+	if res.StatusCode >= 400 {
+		return nil, nil, false
+	}
+	bys, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, bys, false
+	}
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(bys))
+	if err != nil {
+		return doc, bys, false
+	}
+	return doc, bys, true
 }

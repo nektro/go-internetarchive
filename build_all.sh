52f641f9cab9e0177681886ea41df0b5223e23f8
Meghan Denny <hello@nektro.net>
2021-08-29 14:02:22 -0700

    build_all.sh- remove a bunch of unused targets

--- build_all.sh
+++ build_all.sh
@@ -15,41 +15,18 @@ build_template() {
 
 # go tool dist list
 
-build_template aix ppc64
-build_template darwin 386
 build_template darwin amd64
-# build_template darwin arm
-# build_template darwin arm64
 build_template dragonfly amd64
-build_template freebsd 386
 build_template freebsd amd64
-build_template freebsd arm
 build_template freebsd arm64
 build_template illumos amd64
-build_template linux 386
 build_template linux amd64
-build_template linux arm
 build_template linux arm64
-build_template linux mips
-build_template linux mips64
-build_template linux mips64le
-build_template linux mipsle
-build_template linux ppc64
-build_template linux ppc64le
 build_template linux riscv64
-build_template linux s390x
-build_template netbsd 386
 build_template netbsd amd64
-build_template netbsd arm
 build_template netbsd arm64
-build_template openbsd 386
 build_template openbsd amd64
-build_template openbsd arm
 build_template openbsd arm64
-# build_template plan9 386
-# build_template plan9 amd64
-# build_template plan9 arm
 build_template solaris amd64
-build_template windows 386 .exe
 build_template windows amd64 .exe
 build_template windows arm .exe
